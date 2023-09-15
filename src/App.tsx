9d4b03a2821807c36a5b07c2a378664cdd4cdbad
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-09-15 15:44:27 +0200

    add api fetch functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,62 @@
+import { useState } from 'react';
+import './App.scss';
 
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.scss'
+export default function App() {
+  const [location, setLocation] = useState<string>('');
+  const [weatherInfo, setWeatherInfo] = useState<string | null>(null);
 
-function App() {
+  const apiKeyPositionstack = '';
+  const apiKeyOpenWeatherMap = '';
 
+
+
+  const handleSearch = async () => {
+    try {
+      // get geolocation data using geolocation api
+      const response1 = await fetch(
+        `http://api.positionstack.com/v1/forward?access_key=${apiKeyPositionstack}&query=${location}`
+      );
+      const data1 = await response1.json();
+
+      if (data1.data.length > 0) {
+        const { latitude, longitude } = data1.data[0];
+
+        // use latitiude and longitude data from geolocation api to get weather data from weather api
+        const response2 = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKeyOpenWeatherMap}`
+        );
+        const weatherData = await response2.json();
+
+        // display weather data
+        const temperature = Math.round(weatherData.main.temp - 273.15); // Convert from Kelvin to Celsius
+        const description = weatherData.weather[0].description;
+        setWeatherInfo(`Location: ${location}<br>Temperature: ${temperature}°C<br>Condition: ${description}`);
+      } else {
+        setWeatherInfo('Location not found.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
   return (
-    <>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
-    
-    </>
-  )
+    <div>
+      <label htmlFor="locationInput">Enter Location:</label>
+      <input
+        type="text"
+        id="locationInput"
+        placeholder="Enter a city or location"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+      />
+      <button id="searchButton" onClick={handleSearch}>
+        Get Weather
+      </button>
+      <div id="weatherInfo" dangerouslySetInnerHTML={{ __html: weatherInfo || '' }}></div>
+
+      <p>Welcome to GetSunny.</p>
+    </div>
+  );
 }
 
-export default App
+
