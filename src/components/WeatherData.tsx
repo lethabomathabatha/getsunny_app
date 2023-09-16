a32fc2b248ae31e778fbc1e1da663ea69e56d8fe
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-09-16 14:27:10 +0200

    add WeatherData component and styling

--- src/components/WeatherData.tsx
+++ src/components/WeatherData.tsx
@@ -8,8 +8,8 @@ export default function WeatherData() {
 //   const [location, setLocation] = useState<string>('');
 //   const [weatherInfo, setWeatherInfo] = useState<string | null>(null);
 //   const [formattedDate, setFormattedDate] = useState<string | null>(null);
-//   const apiKeyPositionstack = '8dffbdec1cdc02cc5b5e2cbfb62bdb69';
-//   const apiKeyOpenWeatherMap = '4d3cdd60814fc80ba5dc03352851748f';
+//   const apiKeyPositionstack = '';
+//   const apiKeyOpenWeatherMap = '';
 
 // console.log(apiKeyOpenWeatherMap)
 // console.log(apiKeyPositionstack)
