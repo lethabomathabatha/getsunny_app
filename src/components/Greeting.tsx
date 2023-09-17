329702872adf142515f0ed889dd6590d068314c3
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-09-17 16:51:22 +0200

    add Greeting styling

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,39 +1,38 @@
 import { useEffect, useRef, useState } from 'react';
-import './App.scss';
+import whiteSunIcon from '../assets/icon_sunny_small_black.svg';
+import blackMoonIcon from '../assets/icon_moon_small_black.svg'
+
 
 export default function Greeting() {
-   // time-based greeting message
-   const [greetingIcon, setGreetingIcon] = useState()
+  const [greeting, setGreeting] = useState<string>('');
+  const [icon, setIcon] = useState<string | null>(null);
 
-   // using useRef vs regular variable stores the greeting without forcing too many re-renders
-   const greetingRef = useRef<string>(''); 
- 
-   useEffect(() => {
-     const time = new Date().getHours();
-     if (time >= 3 && time < 12) {
-       greetingRef.current = `Good Morning`;
-       setGreetingIcon()
-     } else if (time >= 12 && time < 18) {
-       greetingRef.current = `Good Afternoon`;
-       setGreetingIcon()
-     } else if (time >= 18 && time < 24) {
-       greetingRef.current = `Good Evening`;
-       setGreetingIcon()
-     } else {
-       greetingRef.current = `Good Night`;
-       setGreetingIcon()
-     }
-   }, []);
+  const greetingRef = useRef<string>('');
+
+  useEffect(() => {
+    const time = new Date().getHours();
+    if (time >= 3 && time < 12) {
+      greetingRef.current = 'Good Morning';
+      setIcon(whiteSunIcon); 
+    } else if (time >= 12 && time < 18) {
+      greetingRef.current = 'Good Afternoon';
+      setIcon(whiteSunIcon);
+    } else if (time >= 18 && time < 24) {
+      greetingRef.current = 'Good Evening';
+      setIcon(blackMoonIcon);
+    } else {
+      greetingRef.current = 'Good Night';
+      setIcon(blackMoonIcon);
+    }
+    setGreeting(greetingRef.current);
+  }, []);
 
   return (
     <div className=''>
-      
       <div className=''>
-      <span className=''>{greetingRef.current}{greetingIcon}</span>
+        <span className=''>{greeting}</span>
+        {icon && <img src={icon} style={{ width: '20px', paddingBottom: '15px' }}  alt='icon' />} 
       </div>
-      
     </div>
   );
 }
-
-
