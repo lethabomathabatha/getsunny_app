32267614f0add7f37c21159a87f0424748079a6e
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-09-17 11:56:03 +0200

    add Background component

---
+++ src/components/Background.tsx
@@ -0,0 +1,16 @@
+// import landingImage from '../assets/image_landing.svg'
+import '../App.scss';
+
+export default function Landing() {
+  
+
+  return (
+    <div className='background_container'>
+        <p style={{color: 'white', fontSize: '40px'}}>Cape Town</p>
+        <p style={{color: 'white', fontSize: '40px'}}>Cape Town</p>
+        <p style={{color: 'white', fontSize: '40px'}}>Cape Town</p>
+    </div>
+  );
+}
+
+
