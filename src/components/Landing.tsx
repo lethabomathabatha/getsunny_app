279f5e50d49a5cf5770ba1f98d92bd286710cb0e
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-09-17 11:55:19 +0200

    add Landing page styling

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,14 +9,18 @@ export default function Landing() {
     <div className='main_container'>
     <Search />
     
-        <div className='landing_text'>
+        <div className='landing_container'>
          <img 
           src={landingImage} 
           alt="sound icon" 
           className='landing_image'
         />
-        <span className='landing_text_sub1'>Don't get caught in the wind.</span>
-        <span className='landing_text_sub2'>Get Sunny.</span>
+        <div className='landing_text'>
+            <span className='landing_text_sub1'>Don't get caught in the wind.</span>
+            <br />
+            <span className='landing_text_sub2'>Get Sunny.</span>  
+        </div>
+        
        
         </div>
         
