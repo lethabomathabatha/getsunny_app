f54229cd3584bd264f4b2af29dfc30beb8b87d98
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2023-09-18 14:30:12 +0200

    add background component

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,5 +1,6 @@
 // import landingImage from '../assets/image_landing.svg'
 import sunnyLargeOrange from '../assets/icon_sunny_large_orange.svg'
+import whiteSunIcon from '../assets/icon_sunny_small_white.svg'
 import '../App.scss';
 
 export default function Landing() {
@@ -12,9 +13,18 @@ export default function Landing() {
           alt="sunny large icon" 
           className='sunny_large_icon'
         />
+
+        <div className='background_weather_location'>
+          <p style={{color: 'white', fontSize: '25px'}}>Cape Town </p>
+          <img src={whiteSunIcon} alt="white sun icon" className='weather_details_icon'/>
+        </div>
+       
         <p style={{color: 'white', fontSize: '40px'}}>Cape Town</p>
-        <p style={{color: 'white', fontSize: '40px'}}>Cape Town</p>
-        <p style={{color: 'white', fontSize: '40px'}}>Cape Town</p>
+
+        <div className='background_weather_location'>
+          <p style={{color: 'white', fontSize: '25px'}}>Cape Town </p>
+          <img src={whiteSunIcon} alt="white sun icon" className='weather_details_icon'/>
+        </div>
     </div>
   );
 }
